a47edca9e6d5f2f3b47a9fea0d81f3f7ef2645cc
Evandro Maurina <evandro.maurina@gmail.com>
2021-10-01 20:20:25 -0300

    Comentario

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Hackathon</router-link> |
+      <router-link to="/about">Decola</router-link>
     </div>
     <router-view/>
   </div>
